dfa4be2bc7b2c2b1c8df8307054954336bc4cffc
Ida Bagus Mas Manuaba <64398534+idabgsmas@users.noreply.github.com>
2021-12-14 09:53:47 +0800

    Untuk Login

--- tugas besar/login.c
+++ tugas besar/login.c
@@ -1,41 +1,303 @@
-#include <stdio.h>  //digunakan untuk memanggil isi yang berada di library ini
-#include <stdlib.h> //digunakan agar bisa menggunakan fungsi define
+//Header yang di gunakan untuk standar input output pada bahasa C
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-int main(void)
-{
-    // deklarasi variabel untuk bagian login
-    char username[10];
+//Deklarasi variable global
+int pilihanmasuk;
+time_t waktuserver;                   //variavel waktuserver   => berfungsi untuk mengambil waktu dari sistem.
+
+// Struct User untuk menyimpan member nama, username, password
+typedef struct {
+    char nama[50];
+    char username[12];
     char password[10];
-    int ulang = 0;
+}User;
 
-    // fungsi untuk proses login
-    for (int i = 0; i < 3; i++)
-    {
-        printf("\n==========Silahkan Login Untuk Melanjutkan===========\n");
-        printf("\nmasukan username petugas : \n");
-        scanf("%s", username);
-        printf("Masukan Password Petugas : \n");
-        scanf("%s", password);
+//Mendeklarasikan variabel u pada struct User
+User u;
 
-        // menetapkan data yang digunakan untuk login
-        if ((strcmp(username, "admin") == 0) && (strcmp(password, "admin") == 0))
-        {
-            break;
-        }
-        else
-        {
-            printf("Maaf password atau username anda salah,silahkan coba lagi!!!\n");
-            ulang = ulang + 1;
-        }
+//Deklarasi fungsi-fungsi yang digunkan pada program game puzzle
+void header ();
+void error_alert();                                  /* Merupakan fungsi yang digunakan untuk menampilan pemberitahuan ketika user menginputkan
+                                                        pilihan yang salah pada setiap switch case. */
+void menu_masuk();                                   // Merupakan fungsi yang digunakan untuk menampilkan pilihan menu masuk Login atau Regristrasi
+void waktu();                                        // Merupakan fungsi yang digunakan untuk menampilkan waktu pada sistem tempat user mengakses
+void daftar();                                       /* Fungsi void daftar() untuk menyimpan perintah-perintah yang dibutuhkan
+                                                        untuk registrasi yang akan menyimpan nama, username, serta password yang
+                                                        akan digunakan oleh pengguna*/
+
+void masuk();                                        /* Fungsi void masuk() digunakan agar pengguna dapat masuk dengan username serta
+                                                        password yang sudah dibuat dan nantinya akan dilanjutkan kedalam fitur program*/
+                                                        
+// assign namaFile (nF) agar menyimpan string "RecordLogin.txt"
+char namaFile[] = "RecordLogin.txt";
+
+// fungsi main () adalah kepala dari program ini, dimana program akan di eksekusi oleh fungsi main ini.
+int main (){
+    header();
+    system ("cls");
+    menu_masuk();
+}
+
+void header (){
+    system ("cls");
+	printf  ("\t\t\t\t\t====================================================\n");
+	printf  ("\t\t\t\t\t||                   GAME PUZZLE                  ||\n");
+	printf  ("\t\t\t\t\t====================================================\n");
+	printf  ("\t\t\t\t\t||                Program dibuat oleh:            ||\n");
+	printf  ("\t\t\t\t\t||              1. IDA BAGUS MAS MANUABA          ||\n");
+	printf  ("\t\t\t\t\t||              2. AGUS PUTRA                     ||\n");
+	printf  ("\t\t\t\t\t====================================================\n");
+	printf  ("\t\t\t\t\t||       Tekan ENTER  untuk melanjutkan....       ||\n");
+    printf  ("\t\t\t\t\t====================================================\n");
+	getchar ();
+    system ("cls");
+}
+
+//=======================================================================//
+//***************  Fungsi Untuk Menampilkan Menu Masuk  *****************//
+//=======================================================================//
+// Nama Fungsi    : menu_masuk                                           //
+// Input Argumen  : int pilihanmasuk                                     //
+// Output Argumen : -                                                    //
+// Deskripsi      : Fungsi untuk menampilkan menu awal game  //
+//                  puzzle yaitu menu login, regristrasi dan exit        //
+//                                                                       //
+// Versi : 1.0                                      Rev. 0               //
+// Tgl   : 09-12-2021                               Tgl: -               //
+// Ida Bagus Mas Manuaba - 2105551008                                    //
+// Kelas A                                                               //
+//=======================================================================//
+void menu_masuk(){
+	mnmasuk:
+	printf ("\t\t\t\t\t|================================================|\n");
+    printf ("\t\t\t\t\t|          SELAMAT DATANG DI GAME PUZZLE         |\n");
+    printf ("\t\t\t\t\t|  SILAHKAN LOGIN/REGRISTASI UNTUK DAPAT BERMAIN |\n");    
+    printf ("\t\t\t\t\t|================================================|\n");
+    printf ("\t\t\t\t\t|   No  |    MENU LOGIN/REGISTER                 |\n");
+    printf ("\t\t\t\t\t|-------|----------------------------------------|\n");
+    printf ("\t\t\t\t\t|   1   |    Login                               |\n");
+    printf ("\t\t\t\t\t|   2   |    Registrasi                          |\n");   
+    printf ("\t\t\t\t\t|   4   |    Exit                                |\n");
+    printf ("\t\t\t\t\t|================================================|\n");
+    printf ("\t\t\t\t\t|================================================|\n");
+    printf ("\t\t\t\t\t|| Masukkan pilihan Anda : ");
+    scanf  ("%d", &pilihanmasuk);
+    printf ("\t\t\t\t\t|================================================|\n");
+    // Membersihkan Layar terminal 
+    system ("cls");
+    
+    /* Percabangan Switch dengan kondisi nilai yang disimpan pada variabel
+       pilihan jika pengguna menuliskan 1 maka akan dipanggil fungsi masuk()
+       jika pengguna menuliskna 2 maka akan dipanggil fungsi registrasi()    */
+    switch (pilihanmasuk){
+    case 1:
+        // Jika pengguna menginput angka 1 maka dipanggil fungsi masuk
+        masuk();
+        break;
+    case 2:
+        // Jika pengguna menginput angka 2 maka dipanggil fungsi registrasi
+        daftar();
+        break;
+    case 3:
+    	// Jika pengguna menginput angka 3 maka akan keluar dari program
+        exit(1);
+        break;
+    default:
+        error_alert();
+        // Kembali ke label mnmasuk jika terjadi kesalahan dalam menginput pilihan
+        goto mnmasuk;
+        break;
     }
-    if (ulang < 3)
+}
+
+//=======================================================================//
+//********************* Fungsi Untuk  Daftar Akun ***********************//
+//=======================================================================//
+// Nama Fungsi    : void daftar()                           		     //
+// Input Argumen  : char l.username[12], char l.password[10]             //
+// Deskripsi      : Fungsi ini ditujukan untuk menginput username,       //
+//                  serta password untuk disimpan pada file              //
+//                  RecordLogin.txt yang nantinya akan divalidasi saat     //
+//                  login                                    	         //
+// Versi : 1.0                                      Rev. 0               //
+// Tgl   : 09-12-2021                               Tgl: -               //
+// Ida Bagus Mas Manuaba - 2105551008                                    //
+// Kelas A                                                               //
+//=======================================================================//
+void daftar(){
+    // Membuat pointer regis untuk file "RecordLogin.txt"
+    FILE *regis;
+
+    // Membuka file "RecordLogin.txt" dengan mode "w"
+    regis = fopen(namaFile,"w");
+
+    // Melakukan Pengecekan apakah pointer regis menunjuk kepada file yang dituju ("RecordLogin.txt")
+    if (regis == NULL)
     {
-        // diisi kondisi yang bisa dijalankan setelah user login
-        printf("Anda berhasil login\n");
+        fputs("ERROR, File tidak ditemukan!", stderr);
+        exit(1);
     }
-    else // ini berfungsi jika user tidak login maka akan muncul pesan seperti ini
-    {
-        printf("\nMaaf Anda sudah melebihi batas percobaan login, silahkan login kembali nanti\n");
+
+    // Instruksi untuk menginput nama, username dan password
+    printf("\n\t\t\t\t\t Silahkan masukan data dibawah untuk Regristrasi\n\n");
+    getchar();
+    printf("\t\t\t\t\t Nama : ");
+    gets(u.nama);
+    printf("\n\t\t\t\t\t Enter Username:");
+    scanf ("%s", u.username);
+    printf("\n\t\t\t\t\t Enter Password:");
+    scanf ("%s", u.password);
+
+    // Menuliskan nama yang diinput oleh pengguna kedalam file "RecordLogin.txt"
+    fwrite(&u,sizeof(u),1,regis);
+    fclose(regis);
+    printf("\n\t\t\t\t\t Hallo %s!\n", u.nama);
+    printf("\n\t\t\t\t\t Registrasi Telah Berhasil!");
+    printf ("\n\t\t\t\t\t Akun anda terdaftar, silahkan login dan bersenang-senang !");
+    printf  ("\t\t\t\t\t====================================================\n");
+    getchar();
+	printf  ("\t\t\t\t\t         Tekan ENTER  untuk melanjutkan....    \n");
+	getchar ();
+    // Membersikan layar terminal
+    system ("cls");
+    // Memanggil fungsi masuk()
+    masuk();
+}
+
+//=======================================================================//
+//********* Fungsi Untuk Login dan Regis Akun serta Username ************//
+//=======================================================================//
+// Nama Fungsi    : void masuk()                            		     //
+// Input Argumen  : char username[12], char password[10]                 //
+// Output Argumen : l.username, l.password                               //
+// Deskripsi      : Fungsi ini ditujukan untuk menginput username,       //
+//                  serta password untuk berlanjut ke pada program       //
+//                  Game Puzzle			                       	         //
+// Versi : 1.0                                      Rev. 0               //
+// Tgl   : 09-12-2021                               Tgl: -               //
+// Ida Bagus Mas Manuaba - 2105551008                                    //
+// Kelas A                                                               //
+//=======================================================================//
+void masuk(){
+    /*  Variabel username[12] digunakan untuk menyimpan input username
+        yang dilakukan oleh pengguna. Inputan tersebut akan divalidasi
+        dengan username yang sudah terdaftar pada file RecordLogin.txt   */
+    char username[12];
+
+    /*  Variabel password[10] digunakan untuk menyimpan input password
+        yang dilakukan oleh pengguna. Inputan tersebut akan divalidasi
+        dengan password yang sudah terdaftar pada file RecordLogin.txt   */
+    char password[10];
+
+    // Variabel pilihan untuk pilihan user apakah ingin mendaftar atau keluar
+    int pilihan;
+
+    // Membuat pointer record untuk menunjuk pada file "RecordLogin.txt"
+    FILE *record;
+    // Membuka file dengan mode "r"
+    record = fopen (namaFile, "r");
+    if (record == NULL){
+        fputs ("Error, Tidak terdapat File untuk penyimpanan akun\n", stderr);
+        daftar();
     }
+
+    // Memperlihatkan tanggal login
+    waktu();
+    // Instruksi untuk LOGIN
+    printf ("\t\t\t\t\t||========================||\n");
+    printf ("\t\t\t\t\t||          LOGIN         ||\n");
+    printf ("\t\t\t\t\t||========================||\n");
+    printf ("\t\t\t\t\t|| Username: ");
+    scanf  ("%s", username);
+    printf ("\t\t\t\t\t||========================||\n");
+    printf ("\t\t\t\t\t|| Password: ");
+    scanf  ("%s", password);
+    printf ("\t\t\t\t\t||========================||\n");
+    system ("cls");
+    // Membeaca file "RecordLogin.txt"
+    while(fread(&u,sizeof(u),1,record)){
+        /*  Mengkomparasi username serta password yang diinput pengguna kedalam fungsi
+            masuk() dengan username serta password yang sudah terdaftar */
+        if(strcmp(username, u.username)==0 && strcmp(password, u.password)==0){
+            printf ("\t\t\t\t\t|==================|\n");
+            printf ("\t\t\t\t\t|| Login Berhasil!||\n");
+            printf ("\t\t\t\t\t|==================|\n");
+
+        }
+        //  Jika akun tidak terdaftar maka akan diberikan pilihan untuk mendaftar atau keluar dari program
+        else {
+            printf ("\t\t\t\t\t|=====================||\n");
+            printf ("\t\t\t\t\t||Akun tidak terdaftar!\n");
+            printf ("\t\t\t\t\t|=====================||\n");
+
+            mnmasuk:
+            printf ("\t\t\t\t\t|===========================||\n");
+            printf ("\t\t\t\t\t||Ketik 1 Untuk Regristrasi");
+            printf ("\n\t\t\t\t||Ketik 2 Untuk Login Ulang");
+            printf ("\n\t\t\t\t||Ketik 3 Untuk Exit\n");
+            printf ("\t\t\t\t\t|===========================||\n");
+            printf ("\n\t\t\t\t||Masukan Pilihan: ");
+            scanf ("%d", &pilihan);
+            system ("cls");
+            // Jika pengguna mengetik nomor 1 maka akan dialihkan kepada fungsi daftar
+            if (pilihan == 1){
+                daftar();
+            /* Menggunakan fungsi recursive kepada fungsi masuk agar pengguna
+               dapat melakukan percobaan masuk kembali jika terjadi kesalahan penulisan */
+            } else if (pilihan==2) {
+                masuk();
+            } else if (pilihan ==3){
+                exit(1);
+            } else {
+                printf ("\t\t\t\t\t ERROR");
+                goto mnmasuk;
+            }
+        }
+    }
+    fclose(record);
+    return;
+}
+
+//=======================================================================//
+//**************   Fungsi Untuk Menampilkan Pesan Eror   ****************//
+//=======================================================================//
+// Nama Fungsi    : -                                                    //
+// Input Argumen  : -                                                    //
+// Output Argumen : -                                                    //
+// Deskripsi      : Menampilkan Pemberitahuan Bahwa user memasukan input //
+//                  bahwa user salah memasukkan pilihan.                 //
+//                                                                       //
+// Versi : 1.0                                      Rev. 0               //
+// Tgl   : 09-12-2021                               Tgl: -               //
+// Ida Bagus Mas Manuaba - 2105551008                                    //
+// Kelas A                                                               //
+//=======================================================================//
+void error_alert(){
+    printf   ("\t\t\t\t\t================================================  \n");
+    printf   ("\t\t\t\t\t||           Pilihan Tidak Tersedia!          || \n");
+    printf   ("\t\t\t\t\t||   Silahkan Memilih Pilihan yang Sudah ada  || \n");
+    printf   ("\t\t\t\t\t================================================  \n");
+}
+
+//=======================================================================//
+//***********     Fungsi Untuk Menampilkan Waktu Sistem     *************//
+//=======================================================================//
+// Nama Fungsi    : waktu                                                //
+// Input Argumen  : -                                                    //
+// Output Argumen : -                                                    //
+// Deskripsi      : Fungsi ini digunakan untuk mengambil dan menampilkan //
+//                 Waktu sistem pada saat user mengakses program.        //
+//                                                                       //
+// Versi : 1.0                                      Rev. 0               //
+// Tgl   : 09-12-2021                               Tgl: -               //
+// Ida Bagus Mas Manuaba - 2105551008                                    //
+// Kelas A                                                               //
+//=======================================================================//
+void waktu(){
+    time( & waktuserver);
+    struct tm * waktu = localtime( & waktuserver);
+    printf("\n\t\t\t\t\t||================Tanggal: %i/%i/%i==============||\n\n", waktu -> tm_mday, waktu -> tm_mon + 1, waktu -> tm_year + 1900);
 }
