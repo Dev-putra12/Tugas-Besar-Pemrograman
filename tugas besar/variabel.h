4ed216fe66c1cf46b25d8a671316a594919b6452
aguss1204 <50066231+aguss1204@users.noreply.github.com>
2021-12-28 02:27:36 +0800

    melakukan beberapa perubahan

--- tugas besar/variabel.h
+++ tugas besar/variabel.h
@@ -1,17 +1,17 @@
 //Header untuk menyimpan variable-variable yang digunakan pada program ini
-#include <time.h>       // memanggil seluruh library yang mengandung waktu
-#include "windows.h"      // memanggil seluruh library yang tersedia di windows.h
-#include <string.h>     // memanggi seluruh library string.h
+#include <time.h>// memanggil seluruh library yang mengandung waktu
+#include "windows.h"// memanggil seluruh library yang tersedia di windows.h
+#include <string.h>// memanggi seluruh library string.h
 
 
 
 //Deklarasi variable global
-int pilihanmenu; 			//variabel untuk pilihan menu pada program
-int level;						// variabel untuk level permainan
-int pilihanmasuk;     //variabel untuk pilihan masuk
-time_t waktuserver;   //variabel waktuserver   => berfungsi untuk mengambil waktu dari sistem.
+int pilihanmenu;//variabel untuk pilihan menu pada program
+int level;// variabel untuk level permainan
+int pilihanmasuk;//variabel untuk pilihan masuk
+time_t waktuserver;//variabel waktuserver   => berfungsi untuk mengambil waktu dari sistem.
 
-char field[fieldSize][fieldSize]; // untuk menentukan baris
-char field2[fieldSize][fieldSize]; // untuk menentukan kolom
-unsigned int seed; 								// digunakan agar tidak ada angka yang bernilai negatif 
-int x, y; 												// mendeklarasikan variabel x dan y
+char field[fieldSize][fieldSize];// untuk menentukan baris
+char field2[fieldSize][fieldSize];// untuk menentukan kolom
+unsigned int seed;// digunakan agar tidak ada angka yang bernilai negatif 
+int x, y;// mendeklarasikan variabel x dan y
