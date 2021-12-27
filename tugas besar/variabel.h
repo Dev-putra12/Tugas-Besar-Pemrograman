240fb48fbe82b043095d5ddd55bcd3450af489f5
aguss1204 <50066231+aguss1204@users.noreply.github.com>
2021-12-28 02:23:30 +0800

    menambahkan beberapa perubahan

--- tugas besar/variabel.h
+++ tugas besar/variabel.h
@@ -1,14 +1,17 @@
 //Header untuk menyimpan variable-variable yang digunakan pada program ini
-#include <time.h>
-#include "windows.h"
+#include <time.h>                                         // memanggil seluruh library yang mengandung waktu
+#include "windows.h"                                       // memanggil seluruh library yang tersedia di windows.h
+#include <string.h>																	        // memanggi seluruh library string.h
 
 
 
-//Deklarasi variable global yang digunakan dalam program puzzle ini
-int pilihanmenu;
-char field[fieldSize][fieldSize];
-char field2[fieldSize][fieldSize];
-unsigned int seed;
-int x, y;
+//Deklarasi variable global
+int pilihanmenu; 																	          //variabel untuk pilihan menu pada program
+int level;																			            // variabel untuk level permainan
 int pilihanmasuk;
-time_t waktuserver;  
+time_t waktuserver;                   											//variabel waktuserver   => berfungsi untuk mengambil waktu dari sistem.
+
+char field[fieldSize][fieldSize]; 													// untuk menentukan baris
+char field2[fieldSize][fieldSize]; 													// untuk menentukan kolom
+unsigned int seed; 																	        // digunakan agar tidak ada angka yang bernilai negatif 
+int x, y; 																			            // mendeklarasikan variabel x dan y
