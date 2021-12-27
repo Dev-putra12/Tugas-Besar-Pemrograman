8601dfdc8a91dcbd9c5b471068232a9fd1cc6bbd
aguss1204 <50066231+aguss1204@users.noreply.github.com>
2021-12-27 23:17:25 +0800

    menambahkan variabel yang digunakan

--- tugas besar/variabel.h
+++ tugas besar/variabel.h
@@ -1,1 +1,14 @@
+//Header untuk menyimpan variable-variable yang digunakan pada program ini
+#include <time.h>
+#include "windows.h"
 
+
+
+//Deklarasi variable global yang digunakan dalam program puzzle ini
+int pilihanmenu;
+char field[fieldSize][fieldSize];
+char field2[fieldSize][fieldSize];
+unsigned int seed;
+int x, y;
+int pilihanmasuk;
+time_t waktuserver;  
